18cc50d593c3eb031dafe126946f5784b2b73160
zhiruoyu90 <zhiruoyu90@163.com>
2018-01-13 11:57:38 +0800

    百度识别文字优化

--- src/main/java/com/baidu/aip/BaiduAipUtils.java
+++ src/main/java/com/baidu/aip/BaiduAipUtils.java
@@ -18,8 +18,11 @@ public class BaiduAipUtils {
 	// 传入可选参数调用接口
 	public static final HashMap<String, String> OPTIONS = new HashMap<String, String>();
 	
-	// 识别矩形区域
-	public static final Rectangle RECTANGLE = new Rectangle(100, 300, 900, 900);
+	// 百万英雄 矩形区域
+	public static final Rectangle MILLION_HERO_RECTANGLE = new Rectangle(100, 300, 900, 900);
+
+	// 默认识别区域
+	public static final Rectangle DEFAULT_RECTANGLE = MILLION_HERO_RECTANGLE;
 	
 	static {
 		// 识别语言类型，默认为CHN_ENG。可选值包括：
@@ -41,22 +44,40 @@ public class BaiduAipUtils {
 		// 是否返回识别结果中每一行的置信度
 		OPTIONS.put("probability", "false");
 	}
-	
-	
+
+	/**
+	 * 按图片路径，识别整张图片
+	 * @param path
+	 * @return
+     */
 	public static String recognize(String path) {
 		JSONObject res = BaiduAipOcr.getInstance().basicGeneral(path, OPTIONS);
 		return processResult(res);
 	}
 	
 	public static String recognizeRegion(String path) throws IOException {
-		return recognizeRegion(path, RECTANGLE);
+		return recognizeRegion(path, DEFAULT_RECTANGLE);
 	}
-	
+
+	/**
+	 * 按图片路径，识别矩形区域
+	 * @param path
+	 * @param rectangle
+	 * @return
+	 * @throws IOException
+     */
 	public static String recognizeRegion(String path, Rectangle rectangle) throws IOException {
 		final JSONObject result = BaiduAipOcr.getInstance().basicGeneral(region(path, rectangle), OPTIONS);
 		return processResult(result);
 	}
-	
+
+	/**
+	 * 截取图片的矩形区域， byte[]二进制数组
+	 * @param path
+	 * @param rectangle
+	 * @return
+	 * @throws IOException
+     */
 	public static byte[] region(String path, Rectangle rectangle) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		Thumbnails
@@ -65,7 +86,12 @@ public class BaiduAipUtils {
 				.size(rectangle.width, rectangle.height).toOutputStream(out);
 		return out.toByteArray();
 	}
-	
+
+	/**
+	 * 处理识别结果
+	 * @param result
+	 * @return
+     */
 	private static String processResult(JSONObject result) {
 		final JSONArray wordsResult = result.getJSONArray("words_result");
 		StringBuilder ret = new StringBuilder();
